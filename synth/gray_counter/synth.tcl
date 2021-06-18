fa53d6712423588750ef44ad6ac4b2cce199654f
Dan Moore <mooredan@suncup.net>
2021-06-17 18:53:16 -0700

    First stab at synthesis with custom standard cell library

--- synth/gray_counter/synth.tcl
+++ synth/gray_counter/synth.tcl
@@ -1,29 +1,36 @@
-yosys -import
-# Synthesis script for yosys created by qflow
-read_liberty -lib -ignore_miss_dir -setattr blackbox /usr/local/share/qflow/tech/osu035/osu035_stdcells.lib
-
+#
 set PROJECT_DIR $::env(PROJECT_DIR)
 puts $PROJECT_DIR
 
-read_verilog  $PROJECT_DIR/rtl/gray_counter.v
+set DESIGN gray_counter
+
+set LIB ${PROJECT_DIR}/char/dlm05_stdcells.lib
+
+yosys -import
+# Synthesis script for yosys created by qflow
+# read_liberty -lib -ignore_miss_dir -setattr blackbox /usr/local/share/qflow/tech/osu035/osu035_stdcells.lib
+read_liberty -lib -ignore_miss_dir -setattr blackbox ${LIB}
+
+read_verilog  $PROJECT_DIR/rtl/${DESIGN}.v
 
 # High-level synthesis
-synth -top gray_counter
+synth -top ${DESIGN}
 # Map register flops
-dfflibmap -liberty /usr/local/share/qflow/tech/osu035/osu035_stdcells.lib
+dfflibmap -liberty ${LIB}
 opt
 
 # Map combinatorial cells, standard script
-abc -exe /usr/local/share/qflow/bin/yosys-abc -liberty /usr/local/share/qflow/tech/osu035/osu035_stdcells.lib -script {+strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,\{D\}}
+abc -exe /usr/local/share/qflow/bin/yosys-abc -liberty ${LIB} -script {+strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,\{D\}}
 flatten
 setundef -zero
 
 clean -purge
 # Output buffering
-iopadmap -outpad BUFX2 A:Y -bits
+# iopadmap -outpad BUFX2 A:Y -bits
+
 # Cleanup
 opt
 clean
 yosys rename -enumerate
-write_verilog  gray_counter_mapped.v
+write_verilog  ${DESIGN}.mapped.v
 stat
