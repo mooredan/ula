5efd453bc534fd4c944b367ab220d359da0719ec
Dan Moore <mooredan@suncup.net>
2021-06-24 08:40:33 -0700

    added tie cells to .lib

---
+++ char/tie0_b/cfg.pl
@@ -0,0 +1,9 @@
+# $cellname = "xor2_a";
+# @sim_results = ("delay.log");
+@output_pins = ("z");
+$max_cap{"z"} = 0.500e-12;
+# @input_pins = ("a", "b");
+# $function{"z"} = "!a*b+a*!b";
+# $area = "216.0";
+
+1;
---
+++ char/tie1_b/cfg.pl
@@ -0,0 +1,9 @@
+# $cellname = "xor2_a";
+# @sim_results = ("delay.log");
+@output_pins = ("z");
+$max_cap{"z"} = 0.500e-12;
+$function{"z"} = "1";
+# @input_pins = ("a", "b");
+# $area = "216.0";
+
+1;
