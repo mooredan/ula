9505c524f3df43a5fdcd2eafdfe2a11237057e10
Dan Moore <mooredan@suncup.net>
2021-07-01 10:12:17 -0700

    moving onto the gray counter

--- OpenRoad_tc3/cmd.tcl
+++ OpenRoad_tc3/cmd.tcl
@@ -27,21 +27,22 @@ read_lef ${process}/${process}_tech.lef
 read_lef ${process}/${process}_std_cell.lef
 read_liberty ${process}/${process}_tt.lib
 
-set DESIGN ringosc
+set DESIGN gray_counter
 
-read_verilog ${DESIGN}.v
+read_verilog ${DESIGN}.mapped.v
 link_design ${DESIGN}
 
 set top_margin 0.0
 set right_margin 0.0 
 set left_margin 0.0
 
+set core_rows 8
+set core_width_tracks [expr 7 * 10.0]
+
 set core_llx  ${left_margin}
 set core_lly  0.0
-set core_width_tracks 30.0
 set core_width [expr ${core_width_tracks} * 1.5]
 set core_urx [expr ${core_llx} + ${core_width}]
-set core_rows 2
 set core_ury [expr ${core_lly} + ${core_rows} * 14.4]
 
 set die_llx 0.0
@@ -72,10 +73,16 @@ make_tracks metal3 -x_offset 0.0  -x_pitch 1.6  -y_offset 0.0 -y_pitch 1.6
 
 set x [expr ${die_llx} + 1.2]
 set y 6.4
-place_pin -pin_name {en}  -layer metal2 -location [list $x $y] -pin_size {2.4 0.9}
-set x [expr ${die_urx} - 1.2]
-set y 6.4
-place_pin -pin_name {out} -layer metal2 -location [list $x $y] -pin_size {2.4 0.9}
+place_pin -pin_name {clk}  -layer metal2 -location [list $x $y] -pin_size {2.4 0.9}
+
+
+for {set i 0} {${i} <= 7} {incr i} {
+
+   set x [expr ${die_urx} - 1.2]
+   set y [expr (${i} * 14.4) + 6.4]
+   set pinname [format "Q\[%d\]" $i]
+   place_pin -pin_name ${pinname} -layer metal2 -location [list $x $y] -pin_size {2.4 0.9}
+}
 
 
 
@@ -89,8 +96,6 @@ tapcell -tapcell_master nwsx -distance 60 -endcap_master nwsx
 pdngen -verbose ${process}.pdn
 
 write_def pdn.def
-
-exit
 
 # global_placement -verbose 3
 global_placement -density 0.90
